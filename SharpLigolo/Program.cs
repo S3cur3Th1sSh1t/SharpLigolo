47c45f355bcb3f9d6016a159131b1e6cae29fb53
S3cur3Th1sSh1t <S3cur3Th1sSh1t@github.com>
2020-12-06 14:08:23 +0100

    Initial commit

---
+++ SharpLigolo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLigolo
+{
+    class Program
+    {
+
+        [DllImport("main.dll", EntryPoint = "maindel")]
+        extern static int maindel(byte[] test);
+
+        static void Main(string[] args)
+        {
+            string argsAsString = String.Join(" ", args);
+            maindel(Encoding.ASCII.GetBytes(argsAsString));
+        }
+
+    }
+}
